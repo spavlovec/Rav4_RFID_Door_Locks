ea42bba98cbce58cee1d192274fa223951320b25
Scott Pavlovec <scottpav@gmail.com>
2022-09-21 19:48:32 -0500

    Add files via upload

---
+++ Rav4_RFID_Door_Locks.ino
@@ -0,0 +1,96 @@
+/*
+ * A simple example to interface with rdm6300 rfid reader using esp8266.
+ * We use hardware uart "Serial" instead of the default software uart driver.
+ *
+ * Note:
+ *     The esp8266 let us use 1.5 uarts:
+ *     Serial=uart0=rx+tx, Serial1=uart1=tx only (rx pin is used as flash io).
+ *     Here we sawp the uart pins so uart0_rx goes to the rdm6300_tx,
+ *     and uart1_tx goes to what used to be uart0_tx-
+ *     so debug message goes from Serial1.print(...) to the pc debug terminal.
+ *     https://github.com/esp8266/Arduino/blob/master/doc/reference.rst#serial
+ *
+ * Connections:
+ *     | esp8266 | nodemcu | rdm6300 | notes                               |
+ *     |---------+---------+---------|-------------------------------------|
+ *     | GPIO-01 | TX      |         | This is TXD0, connect it to GPIO-02 |
+ *     | GPIO-02 | D4      |         | This is TXD1, connect it to GPIO-01 |
+ *     | GPIO-03 | RX      |         | Leave it unconnected for flashing   |
+ *     | GPIO-13 | D7      | TX      | TX level is same 3.3V as esp8266    |
+ *     |         | VU (5V) | VCC     | The rdm6300 must be powered with 5V |
+ *     | GND     | GND     | GND     |                                     |
+ *
+ *     * GPIO-01 to GPIO-02 is for debug terminal output.
+ *
+ * Arad Eizen (https://github.com/arduino12).
+ */
+
+#include <rdm6300.h>                   
+
+#define RDM6300_RX_PIN 3 // can be only 13 - on esp8266 force hardware uart!
+#define READ_LED_PIN 13
+#define DOOR_LOCK_PIN 7                                                                                                                                                                                            
+
+Rdm6300 rdm6300;
+
+void setup()
+{
+	/* Serial1 is the debug! remember to bridge GPIO-01 to GPIO-02 */
+	//Serial.begin(115200);
+
+  pinMode(READ_LED_PIN, OUTPUT);
+  digitalWrite(READ_LED_PIN, LOW);
+  pinMode(DOOR_LOCK_PIN, OUTPUT);
+  digitalWrite(DOOR_LOCK_PIN, LOW);
+
+	rdm6300.begin(RDM6300_RX_PIN);
+
+	//Serial.println("\nPlace RFID tag near the rdm6300...");
+}
+
+void loop()
+{
+	/* get_new_tag_id returns the tag_id of a "new" near tag,
+	following calls will return 0 as long as the same tag is kept near. */
+	if (rdm6300.get_new_tag_id()){
+      digitalWrite(DOOR_LOCK_PIN, 1);
+      digitalWrite(READ_LED_PIN, 1);
+      delay(500);
+      digitalWrite(DOOR_LOCK_PIN, 0);
+      digitalWrite(READ_LED_PIN, 0);
+      delay(500);
+      digitalWrite(DOOR_LOCK_PIN, 1);
+      digitalWrite(READ_LED_PIN, 1);
+      delay(500);
+      digitalWrite(DOOR_LOCK_PIN, 0);
+      digitalWrite(READ_LED_PIN, 0);
+		//Serial.println(rdm6300.get_tag_id(), HEX);
+	  String tagId = String (rdm6300.get_tag_id(), HEX);
+       //   Serial.println(tagId);
+
+    if (tagId == "3454ec") {
+     // Serial.println("Authorized");
+      digitalWrite(DOOR_LOCK_PIN, 1);
+      digitalWrite(READ_LED_PIN, 1);
+      delay(500);
+      digitalWrite(DOOR_LOCK_PIN, 0);
+      digitalWrite(READ_LED_PIN, 0);
+      delay(500);
+      digitalWrite(DOOR_LOCK_PIN, 1);
+      digitalWrite(READ_LED_PIN, 1);
+      delay(500);
+      digitalWrite(DOOR_LOCK_PIN, 0);
+      digitalWrite(READ_LED_PIN, 0);
+    }
+	}
+//  digitalWrite(DOOR_LOCK_PIN, HIGH);
+//  delay(2000);
+//  digitalWrite(DOOR_LOCK_PIN, LOW);
+
+  //3454EC
+
+	/* get_tag_id returns the tag_id as long as it is near, 0 otherwise. */
+	
+
+	delay(10);
+}
